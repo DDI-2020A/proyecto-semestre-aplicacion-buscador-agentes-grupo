f4cc3de5bad70dd53a382351d406552962eeb5df
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-09-02 16:46:10 -0500

    base react project, ant

---
+++ src/AgentList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const agent = {
+    name:'',
+    age:''
+}
+
+
+
+function AgentItem(agent){
+    return(
+        <div></div>
+    )
+}
+
+export default function AgentList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
